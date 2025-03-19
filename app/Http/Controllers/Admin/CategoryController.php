bb28c09a358c0c8a99861b126cadbe4c6c2c9578
saipulanwarr <saipulanwar180994@gmail.com>
2025-03-19 11:35:50 +0700

    added update category

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -40,4 +40,59 @@ class CategoryController extends Controller
 
         return redirect()->route('all.category')->with($notification);
     }
+
+    public function EditCategory($id){
+        $category = Category::find($id);
+
+        return view('admin.backend.category.edit_category', compact('category'));
+    }
+
+    public function UpdateCategory(Request $request){
+        $cat_id = $request->id;
+        $data = Category::find($cat_id);
+
+        $oldPhoto = $data->image;
+
+        if($request->hasFile('image')){
+            $file = $request->file('image');
+            $filename = time().'.'.$file->getClientOriginalExtension();
+            $file->move(public_path('upload/category'), $filename);
+            $save_url = 'upload/category/'.$filename;
+
+            if($oldPhoto && $oldPhoto !== $filename){
+                $this->deleteOldImage($oldPhoto);
+            }
+
+            Category::find($cat_id)->update([
+                'category_name' => $request->category_name,
+                'image' => $save_url,
+            ]);
+
+            $notification = array(
+                'message' => 'Category Inserted successfully',
+                'alert-type' => 'success',
+            );
+    
+            return redirect()->route('all.category')->with($notification);
+        }else{
+            Category::find($cat_id)->update([
+                'category_name' => $request->category_name,
+            ]);
+
+            $notification = array(
+                'message' => 'Category Updated without image successfully',
+                'alert-type' => 'success',
+            );
+    
+            return redirect()->route('all.category')->with($notification);
+        }
+    }
+
+    private function deleteOldImage(string $oldPhotoPath): void {
+        $fullPath = public_path($oldPhotoPath);
+        
+        if(file_exists($fullPath)){
+            unlink($fullPath);
+        }
+    }
 }
