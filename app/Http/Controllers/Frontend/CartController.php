30d3bb904e62470f6167d21c82ccd09c158f3764
saipulanwarr <saipulanwar180994@gmail.com>
2025-05-27 21:23:45 +0700

    product add in cart with price

---
+++ app/Http/Controllers/Frontend/CartController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth; 
+use App\Models\Product;
+use Illuminate\Support\Facades\Session; 
+
+class CartController extends Controller
+{
+    public function AddToCart($id){
+        $products = Product::find($id);
+
+        $cart = session()->get('cart',[]);
+        if (isset($cart[$id])) {
+           $cart[$id]['quantity']++;
+        } else {
+           $priceToShow = isset($products->discount_price) ? $products->discount_price : $products->price;
+           $cart[$id] = [
+            'id' => $id,
+            'name' => $products->name,
+            'image' => $products->image,
+            'price' => $priceToShow,
+            'client_id' => $products->client_id,
+            'quantity' => 1
+           ];
+        }
+        session()->put('cart',$cart);
+
+        // return response()->json($cart);
+        $notification = array(
+            'message' => 'Add to Cart Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
+    }
+
+    public function updateCartQuanity(Request $request){
+        $cart = session()->get('cart',[]);
+
+        if (isset($cart[$request->id])) {
+           $cart[$request->id]['quantity'] = $request->quantity;
+           session()->put('cart',$cart);
+        }
+        $notification = array(
+            'message' => 'Cart Updated Successfully',
+            'alert-type' => 'success'
+        );
+
+        return response()->json([
+            'message' => 'Quantity Updated',
+            'alert-type' => 'success'
+        ]);
+    }
+
+    public function CartRemove(Request $request){
+        $cart = session()->get('cart',[]);
+
+        if (isset($cart[$request->id])) {
+           unset($cart[$request->id]);
+           session()->put('cart',$cart);
+        }
+        $notification = array(
+            'message' => 'Product Remove Successfully',
+            'alert-type' => 'success'
+        );
+
+        return response()->json([
+            'message' => 'Cart Remove Successfully',
+            'alert-type' => 'success'
+        ]);
+     }
+}
