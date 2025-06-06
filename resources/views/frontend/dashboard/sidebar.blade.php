82cd9614ce2f84b935af3500ac5e17cd2d87b38e
saipulanwarr <saipulanwar180994@gmail.com>
2025-06-06 20:31:22 +0700

    manage order from user panel

--- resources/views/frontend/dashboard/sidebar.blade.php
+++ resources/views/frontend/dashboard/sidebar.blade.php
@@ -38,11 +38,10 @@
                         class="icofont-food-cart"></i> Favourites </a>
             </li>
 
-            <li class="nav-item">
+            <li class="nav-item">More actions
                 <a class="nav-link {{ Route::currentRouteName() === 'user.order.list' ? 'active' : '' }}"
-                    href="#" role="tab" aria-controls="orders" aria-selected="true"><i
-                        class="icofont-food-cart"></i> Orders
-                </a>
+                    href="{{ route('user.order.list') }}" role="tab" aria-controls="orders" aria-selected="true"><i
+                        class="icofont-food-cart"></i> Orders </a>
             </li>
 
 
