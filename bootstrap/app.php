ed8d54b90942d8f359fecec225085b4b6a67c2ee
saipulanwarr <saipulanwar180994@gmail.com>
2025-06-07 12:07:36 +0700

    add role and permission for admin user

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,8 +11,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->alias(['admin' => App\Http\Middleware\Admin::class, 'client' => App\Http\Middleware\Client::class, 'status' => App\Http\Middleware\ClientStatus::class
-]);
+        $middleware->alias([
+            'admin' => App\Http\Middleware\Admin::class, 
+            'client' => App\Http\Middleware\Client::class, 
+            'status' => App\Http\Middleware\ClientStatus::class,
+            'permission' => App\Http\Middleware\CheckPermission::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
